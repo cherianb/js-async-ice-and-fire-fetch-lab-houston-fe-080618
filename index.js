83f0538d0a7caad3977539f0b093103c5c74fe95
Cherian Porter <cherianbelun@gmail.com>
2018-09-26 23:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
+fetch('https://anapioficeandfire.com/api/books')
+.then(resp => resp.json())
+.then(json => renderBooks(json));
+
 }
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+function renderBooks(json){
+  let counter = 0
+ const main = document.querySelector('main');
+ json.forEach(book => {
+   
+   
+  if(counter === 4){
+    const fifthBook = document.createElement ('h1');
+   fifthBook.innerHTML =`<h2> This ${book.name} is the 5th book</h2>`
+   main.appendChild(fifthbook)
+
+ })
+ const h2 =document.createElement('h2');
+ h2.innerHTML= 'h2>${book.name}</h2>'
+ main.appendChild(h2)
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener ('DOMContentLoaded', function() {
+ fetchBooks();
+ counter++
+});
+
+// hold on don’t load yet!
